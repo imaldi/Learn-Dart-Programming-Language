092aae6031c659d6eb296f475bb89a5aadd0726c
imaldi <aldiirsanmajid@gmail.com>
2023-01-09 23:42:18 +0700

    done Exceptions

--- belajar_dari_awal_lagi/functions.dart
+++ belajar_dari_awal_lagi/functions.dart
@@ -41,6 +41,9 @@ void main(List<String> args) {
   // pakai konsep "function as first class object"
   hitungAngka(a, b, luasSegitiga);
   // hitungAngka(a, b, contohSignatureBeda);
+
+  // coba throw error
+  division(5, 0);
 }
 
 // deklarasi => ngebuat
@@ -65,7 +68,40 @@ int times(int a, int b) {
   return a * b;
 } // signature nya itu int Function(int, int)
 
+int division(int a, int b) {
+  // Error harusnya ga di taro di dalam try, supaya programmer tau, salah pakai kodingannya di mana
+  if (b == 0) throw UnsupportedError("Pembagian Dengan 0");
+
+  // Exception (Bisa juga sih error. tapi kan tadi error ga d sarankan utk d tangkap, biar lepas aja)
+  try {
+    print("");
+  } on UnsupportedError {
+    // handle kalau pembagian 0
+  }
+  // on HttpException {
+  //   // handle lain lagi caranya
+  //   print("Koneksi ke server gagal");
+  // }
+  catch (e) {
+    print("Errornya adalah: $e");
+    //
+    // jlaksjld
+  } finally {
+    // finally selalu jalan mau program throw error atau catch exception, atau ga ada error/exception sama sekali
+    print("Mau error ataupun ga error tetep jalan gaskan");
+  }
+  return a ~/ b;
+}
+
+@deprecated
 int luasSegitiga(int alas, int tinggi) {
+  ///
+  /// skndlka
+  /// msdma
+  return 0;
+}
+
+int luasSegitigaRumusModer(int alas, int tinggi) {
   return 0;
 }
 
