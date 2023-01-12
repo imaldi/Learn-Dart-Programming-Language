bdbeb1b00cda7b349721b8f290e34b5b8340982e
imaldi <aldiirsanmajid@gmail.com>
2023-01-12 20:22:39 +0700

    terakhir belajar pola pola

---
+++ practice/star_right_triangle.dart
@@ -0,0 +1,7 @@
+var batasAkhir = 7;
+void main(List<String> args) {
+  for (var i = 1; i <= batasAkhir; i++) {
+    var j = '*' * i;
+    print(j);
+  }
+}
---
+++ practice/star_right_triangle_inverse.dart
@@ -0,0 +1,7 @@
+var batasAwal = 10;
+void main(List<String> args) {
+  for (var i = batasAwal; i >= 1; i--) {
+    var j = '*' * i;
+    print(j);
+  }
+}
