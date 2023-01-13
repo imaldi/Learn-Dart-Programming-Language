8404abc03b1a54997e2716fb3d07af8ebedd379e
imaldi <aldiirsanmajid@gmail.com>
2023-01-13 20:36:53 +0700

    oop part 2

--- belajar_dari_awal_lagi/oop/learn_oop_class.dart
+++ belajar_dari_awal_lagi/oop/learn_oop_class.dart
@@ -9,7 +9,7 @@
 
 // kingdom
 class Animal {
-  // property / member
+  // property / Instance variables / member
   String namaPanggilan;
   int jumlahKaki;
   int jumlahMata;
@@ -19,7 +19,7 @@ class Animal {
       required this.jumlahKaki,
       required this.jumlahMata}); // inisasi properties dari class ini
 
-  /// behavior => function
+  /// behavior / method => function / ini juga member
   bersuara(String suaranyaGimana) {
     print("Suaranya yaitu: $suaranyaGimana");
   }
@@ -32,36 +32,71 @@ class Animal {
 }
 
 class Human {
-  String nama;
-  String jenisKelamin;
-  int umur;
-  int tinggiBadan;
-  String? hobi; // kalau ga diisi dia null
-  String? pendidikan;
+  final String nama;
+  final String jenisKelamin;
+  final int umur;
+  final int tinggiBadan;
+  final String? hobi; // kalau ga diisi dia null
+  final String? pendidikan;
   // Constructor untuk pembuatan instance Human
-  Human(this.nama, this.jenisKelamin, this.umur, this.tinggiBadan,
+  // kita buat constructor const, supaya nanti bisa kita bandingin datanya kalau sama
+  // tapi pas inisialisasi, kita tetep harus manggil constructor nya dengan keyword const juga
+  const Human(this.nama, this.jenisKelamin, this.umur, this.tinggiBadan,
       {this.hobi, this.pendidikan});
+
+  /// named constructor
+  Human.european()
+      : nama = "",
+        jenisKelamin = "L",
+        umur = 0,
+        tinggiBadan = 190,
+        hobi = "Mabar",
+        pendidikan = "SD";
+  Human.asian()
+      : nama = "",
+        jenisKelamin = "L",
+        umur = 0,
+        tinggiBadan = 190,
+        hobi = "Healing",
+        pendidikan = "TK";
 }
 // . => operator accesor
 
 // class Vertebrata extends Animal {}
-Animal? anjingPeliharaanIpul;
+late Animal? anjingPeliharaanIpul;
 void main(List<String> args) {
-  Animal? anjingPeliharaanIpul =
+  anjingPeliharaanIpul =
       Animal(namaPanggilan: "Doge", jumlahKaki: 4, jumlahMata: 2);
-  anjingPeliharaanIpul.bersuara("Kwek Kwek");
+  anjingPeliharaanIpul?.bersuara("Kwek Kwek");
+  // bersuara();
   print(anjingPeliharaanIpul);
-  print("Nama si anjing: ${anjingPeliharaanIpul.namaPanggilan}");
-  print("Jumlah kaki si anjing: ${anjingPeliharaanIpul.jumlahKaki}");
-  print("Jumlah mata si anjing: ${anjingPeliharaanIpul.jumlahMata}");
+  print("Nama si anjing: ${anjingPeliharaanIpul?.namaPanggilan}");
+  print("Jumlah kaki si anjing: ${anjingPeliharaanIpul?.jumlahKaki}");
+  print("Jumlah mata si anjing: ${anjingPeliharaanIpul?.jumlahMata}");
 
   var upin = Human("Upin ", "L", 5, 100,
       hobi: "Mancing",
       pendidikan:
           "SD"); // upin dan ipin adalah 2 instance yang berbeda dari class Human
-  var ipin = Human("Ipin ", "L", 5, 100,
+  var ipin = const Human("Ipin ", "L", 5, 100,
       pendidikan: "Tadika Mesra", hobi: "Mancing juga");
-  ipin.hobi = "Mabar";
+  var ipin2 = const Human("Ipin ", "L", 5, 100,
+      pendidikan: "Tadika Mesra", hobi: "Mancing juga");
+  print("Apakah orang yang sama: ${ipin == ipin2}");
+  // ipin.hobi = "Mabar";
   var hobiIpin = ipin.hobi;
   print(hobiIpin);
+
+  print("Tipe data variabel upin: ${upin.runtimeType}");
+
+  /// type casting
+  String a = "02abshsbd3";
+  int? b = int.tryParse(a);
+
+  var orangBule = Human.european();
+  var orangCina = Human.asian();
+
+  if (ipin is Human) {
+    print("Ya, benar");
+  }
 }
